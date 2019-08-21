cc30402332d7aec7baac4432cdcdaa5816ad6289
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-21 15:29:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_pices_owned => 25,
+        :one_pieces_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 75,
+        :four_pieces_owned => 100,
       }
     }
   }
