9262b1c18364cf05cacfba7a63ad678a31172f78
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-21 15:22:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly_set = {:railroads => {:pieces => {}}}
+  monopoly_set = {:railroads => {:pieces => 4}}
 end
 
 def monopoly_with_third_tier
